0bf478946c91620a87c09cca20b46841d3407b62
Aditya-raaz01 <adityaraazofficial2003@gmail.com>
2024-03-23 18:48:39 +0530

    3rd commit

--- src/Components/Dashboard/AddPatient.js
+++ src/Components/Dashboard/AddPatient.js
@@ -2,6 +2,35 @@ import './AddPatient.css'
 import { Link } from "react-router-dom";
 
 export default function addPatient(){
+    const [patientName, setpatientName] = useState("");
+    const [patientAge, setpatientAge] = useState("");
+    const [patientGender, setpatientGender] = useState("");
+    const [patientNumber, setpatientNumber] = useState("");
+
+    function userData() {
+    let data = {
+      patientName,
+      patientGender,
+      patientNumber,
+      patientAge
+      
+    };
+    fetch("http://localhost:8080/user/addPatient", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((resp) => {
+      // console.warn("resp",resp);;
+      resp.json().then((result) => {
+        console.log("result", result);
+      });
+    });
+  }
+
     return(
         <>
             <div id="dashboard">
@@ -13,15 +42,24 @@ export default function addPatient(){
                 </div>
                 <div id="display">
                     <img src='/pictures/addPatientBackImage.jpg'/>
-                    <input type='text' placeholder='Name' id='patientName'></input><br/>
+                    <input type='text' placeholder='Name' id='patientName' name='patientName' value={patientName} onChange={(e)=>{
+                        setpatientName(e.target.value)
+                    }}></input><br/>
                     <span id='patientGender'>Gender: </span>
-                    <select name="cars" id="patientListGender" form="carform">
+                    <select id="patientListGender" name='patientGender' value={patientGender} onChange={(e)=>{
+                        setpatientGender(e.target.value)
+                    }}>
                         <option value="volvo">Male</option>
                         <option value="saab">Female</option>
                     </select>
                     <br/>
-                    <input type='number' placeholder='Age' id='patientAge'></input><br/>
-                    <button id='patientSubmit'>Submit</button>
+                    <input type='number' placeholder='Age' id='patientAge' name='patientAge' value={patientAge} onChange={(e)=>{
+                        setpatientAge(e.target.value)
+                    }}></input><br/>
+                    <input type='text' placeholder='Number' id='patientNumber' name='patientNumber' value={patientNumber} onChange={(e)=>{
+                        setpatientNumber(e.target.value)
+                    }}></input><br/>
+                    <button id='patientSubmit' onClick={userData}>Submit</button>
 
                 </div>
             </div>
--- src/Components/Dashboard/Profile.js
+++ src/Components/Dashboard/Profile.js
@@ -1,20 +1,61 @@
 import { Link } from "react-router-dom";
 import "./Profile.css";
+import { useEffect, useState } from "react";
 
-export default function Profile(){
-    return(
-        <>
-            <div id="dashboard">
-                <div id="panel">
-                    <div id="panelButton"><Link to='/user/profile' id="panelButtonText">Profile</Link></div>
-                    <div id="panelButton"><Link to='/user/addPatient' id="panelButtonText">Add Patient</Link></div>
-                    <div id="panelButton"><Link to='/user/addMedicine' id="panelButtonText">Add Medicine</Link></div>
-                    <div id="panelButton"><Link to='/' id="panelButtonText">Log Out</Link></div>
-                </div>
-                <div id="display">
+export default function Profile() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/user/profile").then((result) => {
+      result.json().then((resp) => {
+        // console.warn(resp)
+        setUser(resp);
+      });
+    });
+  }, []);
+  console.warn(users);
 
+  return (
+    <>
+      <div id="dashboard">
+        <div id="panel">
+          <div id="panelButton">
+            <Link to="/profile" id="panelButtonText">
+              Profile
+            </Link>
+          </div>
+          <div id="panelButton">
+            <Link to="/addPatient" id="panelButtonText">
+              Add Patient
+            </Link>
+          </div>
+          <div id="panelButton">
+            <Link to="/addMedicine" id="panelButtonText">
+              Add Medicine
+            </Link>
+          </div>
+          <div id="panelButton">
+            <Link to="/" id="panelButtonText">
+              Log Out
+            </Link>
+          </div>
+        </div>
+        <div id="display">
+          <div className="App">
+            <h1>Our Elders</h1>
+            {users.map((e) => {
+              <div className="card">
+                <img src="img_avatar.png" alt="Avatar" style="width:100%" />
+                <div className="container">
+                  <h4>
+                    <b></b>
+                  </h4>
+                  <p>Architect & Engineer</p>
                 </div>
-            </div>
-        </>
-    );
+              </div>;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
